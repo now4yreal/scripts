c62d43553ce6d874fa81f2b71369696ef0804cbf
now4yreal <369231613@qq.com>
2021-03-26 18:43:23 -0700

    update

--- pin/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ./source/tools/MyPinTool/
-make obj-intel64/itrace.so TARGET=intel64
-cp obj-intel64/itrace.so ~/ctf/pin-3.7/
-cd ~/ctf/pin-3.7/
-./pin -t itrace.so -- ./my_bin/easyparser #change bin name here
-cat itrace.out
